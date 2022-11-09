5897280337604cdec52429a6d4439a7086f478fc
David Luna <david347lj@gmail.com>
2022-11-09 09:05:24 +0100

    arreglado node

--- backup_web_fuente_NOV_2022/node/index.js
+++
@@ -1,7 +0,0 @@
-var cgi = require('cgi');
-var http = require('http');
-var path = require('path');
-
-var script = path.resolve(__dirname, '../enfecha.cgi');
-
-http.createServer( cgi(script) ).listen(8080,"127.0.0.1", ()=>{console.log("server running")});
