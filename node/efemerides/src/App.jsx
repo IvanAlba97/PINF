80e8f7c5b04dd8310e478ed9c9d3754322361b47
Warbacon <joaquinguerratocino@gmail.com>
2022-11-23 09:39:35 +0100

    Configuración de postcss y fuente Verdana

--- node/efemerides/src/App.jsx
+++ node/efemerides/src/App.jsx
@@ -2,9 +2,13 @@ import './App.css'
 
 function App() {
   return (
+    <>
     <div className="App">
-      
+      <h1>Esto es una prueba
+        <p>Css anidado</p>
+      </h1>
     </div>
+    </>
   )
 }
 
