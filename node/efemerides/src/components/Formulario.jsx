7e5e293503f283b2da4394c6f009e0d0b8972f0e
David Luna Jurado <david@DPC.localdomain>
2022-12-08 12:48:56 +0100

    Agrupado todo el estado del formulario en un objeto

--- node/efemerides/src/components/Formulario.jsx
+++ node/efemerides/src/components/Formulario.jsx
@@ -2,17 +2,33 @@ import { useState } from 'react'
 import './Formulario.css'
 
 export default function Formulario(props) {
-    let [latitud, setLatitud] = useState(0)
-    let [grados, setGrados] = useState(0)
-    let [minutos, setMinutos] = useState(0)
-    let [fecha, setFecha] = useState(null)
-    let [hora, setHora] = useState(0)
 
-    const actualizarLatitud = (estado) => { setLatitud(estado) }
-    const actualizarGrados = (estado) => { setGrados(estado) }
-    const actualizarMinutos = (estado) => { setMinutos(estado) }
-    const actualizarFecha = (estado) => { setFecha(estado) }
-    const actualizarHora = (estado) => { setHora(estado) }
+    const [datos, setDatos] = useState(
+        {
+            latitud: 0,
+            longitud: 0,
+            gradosLat: 0,
+            minutosLat: 0,
+            gradosLong: 0, 
+            minutosLong: 0,
+            fecha: null,
+            hora: 0,
+            norteSur: null, // 0 Norte, 1 Sur
+            esteOeste: null, //0 Este, 1 Oeste
+            
+        }
+    )
+
+    const actualizarLatitud = (estado) => { setDatos({...datos, latitud: estado}) }
+    const actualizarLongitud = (estado) => { setDatos({...datos, longitud: estado}) }
+    const actualizarGradosLat = (estado) => { setDatos({...datos, gradosLat: estado}) }
+    const actualizarMinutosLat = (estado) => { setDatos({...datos, minutos: estado}) }
+    const actualizarGradosLong = (estado) => { setDatos({...datos, gradosLong: estado}) }
+    const actualizarMinutosLong = (estado) => { setDatos({...datos, minutosLong: estado}) }
+    const actualizarFecha = (estado) => { setDatos({...datos, fecha:estado}) }
+    const actualizarHora = (estado) => { setDatos({...datos, hora: estado}) }
+    const actualizarNorteSur = (estado) => { setDatos({...datos, norteSur: estado}) }
+    const actualizarEsteOeste = (estado) => { setDatos({...datos, esteOeste: estado}) }
 
     return (
         <div className="formulario">
@@ -21,9 +37,9 @@ export default function Formulario(props) {
                 <div className='apartado'>
                     <h2 className='posicion'>Posición</h2>
                     <div className='elementos'>
-                        <InputNumber label="Latitud" value={latitud} onChange={actualizarLatitud} />
-                        <InputNumber label="Grados" value={grados} onChange={actualizarGrados} />
-                        <InputNumber label="Minutos" value={minutos} onChange={actualizarMinutos} />
+                        <InputNumber label="Latitud" value={datos.latitud} onChange={actualizarLatitud} />
+                        <InputNumber label="Grados" value={datos.gradosLat} onChange={actualizarGradosLat} />
+                        <InputNumber label="Minutos" value={datos.minutosLat} onChange={actualizarMinutosLat} />
                         <div>
                             <label className='inputLabel'>Orientación</label>
                             <div className='radioGroup'>
@@ -31,6 +47,19 @@ export default function Formulario(props) {
                                 <Radio valor="Sur" name="Nortesur" />
                             </div>
                         </div>
+                    </div>
+                    <div className="elementos">
+                        <InputNumber label="Longitud" value={datos.longitud} onChange={actualizarLongitud} />
+                        <InputNumber label="Grados" value={datos.gradosLong} onChange={actualizarGradosLong} />
+                        <InputNumber label="Minutos" value={datos.longitud} onChange={actualizarMinutosLong} />
+                        <div>
+                            <label className='inputLabel'>Orientación</label>
+                            <div className='radioGroup'>
+                                <Radio valor="Este" name="EsteOeste" />
+                                <Radio valor="Oeste" name="EsteOeste" />
+                            </div>
+                        </div>
+
                     </div>
                     <div className='apartado'>
                         <h2 className='posicion'>Fecha y Hora</h2>
@@ -42,24 +71,23 @@ export default function Formulario(props) {
     )
 }
 function InputNumber({ label, value, onChange }) {
-    const [valor,setValor] = useState('')
+    const [valor, setValor] = useState('')
     const handleChange = (event) => {
         if (!isNaN(event.target.value) && event.target.value !== '') {
             setValor(event.target.value)
             onChange(parseInt(event.target.value))
         }
-        else if(event.target.value === '' )
-        {
+        else if (event.target.value === '') {
             setValor('')
             onChange(0)
         }
     }
 
     return (
-    <div>
-        <label className='inputLabel'>{label}</label>
-        <input type="number" className='input' value={valor} min="0" placeholder="0" onChange={handleChange}></input>
-    </div>
+        <div>
+            <label className='inputLabel'>{label}</label>
+            <input type="number" className='input' value={valor} min="0" placeholder="0" onChange={handleChange}></input>
+        </div>
     )
 }
 
