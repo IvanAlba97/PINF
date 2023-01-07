0988249893f5b98ca888185b9feb153bb303f981
Gcrows <64683398+Gcrows@users.noreply.github.com>
2023-01-07 15:05:07 +0100

    tablas de respuesta

--- node/efemerides/src/components/Respuesta.jsx
+++ node/efemerides/src/components/Respuesta.jsx
@@ -1,19 +1,161 @@
+import './Respuesta.css'
+
 export default function Respuesta({ datos, intervalo }) {
   let tabla = datos.tabla1;
-  console.log(datos);
 
-  if(intervalo)
-  {
-    console.log(datos);
-    return <h1>Petición a Intervalo</h1>
+  if (intervalo) {
+    let interval = datos.DatosFecha;
+    console.log(interval);
+
+    if (interval[0].SalidaLimboSuperior) {
+      return (
+        <div id="contenedor">
+          <div className="fecha">
+            Intervalo: {datos.diaI}/{datos.mesI}/{datos.anoI} - {datos.diaF}/
+            {datos.mesF}/{datos.anoF}
+          </div>
+          <table id="coordenadas">
+            <tr>
+              <th id="header">Hora-UT</th>
+              <td>{tabla.horut}h</td>
+            </tr>
+            <tr>
+              <th id="header">Latitud</th>
+              <td>
+                {tabla.latgra}º {tabla.minutos_lat}' {tabla.latsig}
+              </td>
+            </tr>
+            <tr>
+              <th id="header">Longitud</th>
+              <td>
+                {tabla.longra}º {tabla.minutos_lon}' {tabla.lonsig}
+              </td>
+            </tr>
+          </table>
+
+          <table className="intervalo">
+            <tr>
+              <th id="header"></th>
+              <th id="header">Orto</th>
+              <th id="header">Meridiano</th>
+              <th id="header">Ocaso</th>
+            </tr>
+            {interval.map((dia) => (
+              <tr>
+                <th>
+                  {dia.dia}/{dia.mes}/{dia.ano}
+                </th>
+                <td>
+                  {dia.SalidaLimboSuperior.hora}:
+                  {dia.SalidaLimboSuperior.minutos}h
+                </td>
+                <td>
+                  {dia.PasoMeridiano.hora}:{dia.PasoMeridiano.minutos}h
+                </td>
+                <td>
+                  {dia.PuestaLimboSuperior.hora}:
+                  {dia.PuestaLimboSuperior.minutos}h
+                </td>
+              </tr>
+            ))}
+          </table>
+        </div>
+      );
+    } else if (interval[0].Salida) {
+      return (
+        <div id="contenedor">
+          <div className="fecha">
+            Intervalo: {datos.diaI}/{datos.mesI}/{datos.anoI} - {datos.diaF}/
+            {datos.mesF}/{datos.anoF}
+          </div>
+          <table id="coordenadas">
+            <tr>
+              <th id="header">Hora-UT</th>
+              <td>{tabla.horut}h</td>
+            </tr>
+            <tr>
+              <th id="header">Latitud</th>
+              <td>
+                {tabla.latgra}º {tabla.minutos_lat}' {tabla.latsig}
+              </td>
+            </tr>
+            <tr>
+              <th id="header">Longitud</th>
+              <td>
+                {tabla.longra}º {tabla.minutos_lon}' {tabla.lonsig}
+              </td>
+            </tr>
+          </table>
+
+          <table className="intervalo">
+            <tr>
+              <th id="header"></th>
+              <th id="header">Orto</th>
+              <th id="header">Meridiano</th>
+              <th id="header">Ocaso</th>
+              <th id="header">Edad</th>
+            </tr>
+            {interval.map((dia) => (
+              <tr>
+                <th>
+                  {dia.dia}/{dia.mes}/{dia.ano}
+                </th>
+                <td>
+                  {dia.Salida.hora || dia.Salida.minutos
+                    ? dia.Salida.hora + ":" + dia.Salida.minutos + "h"
+                    : "----"}
+                </td>
+                <td>
+                  {dia.PasoMeridiano.hora || dia.PasoMeridiano.minutos
+                    ? dia.PasoMeridiano.hora +
+                      ":" +
+                      dia.PasoMeridiano.minutos +
+                      "h"
+                    : "----"}
+                </td>
+                <td>
+                  {dia.Puesta.hora || dia.Puesta.minutos
+                    ? dia.Puesta.hora + ":" + dia.Puesta.minutos + "h"
+                    : "----"}
+                </td>
+                <td>{dia.edad}</td>
+              </tr>
+            ))}
+          </table>
+        </div>
+      );
+    }
   }
-
 
   if (datos.ERROR) {
-    return <h1>Error: {datos.ERROR}</h1>;
+    return (
+      <div id="contenedor">
+        <div className="fecha">
+          Fecha: {tabla.diai}/{tabla.mesi}/{tabla.anoi}
+        </div>
+        <table id="coordenadas">
+          <tr>
+            <th id="header">Hora-UT</th>
+            <td>{tabla.horut}h</td>
+          </tr>
+          <tr>
+            <th id="header">Latitud</th>
+            <td>
+              {tabla.latgra}º {tabla.minutos_lat}' {tabla.latsig}
+            </td>
+          </tr>
+          <tr>
+            <th id="header">Longitud</th>
+            <td>
+              {tabla.longra}º {tabla.minutos_lon}' {tabla.lonsig}
+            </td>
+          </tr>
+        </table>
+        <h1 className="ERROR">{datos.ERROR}</h1>
+      </div>
+    );
   }
-  if(datos.Sol)
-  {
+  if (datos.Sol) {
     let Sol = datos.Sol;
     let Fenomenos = datos.fenomenoSol;
 
@@ -21,85 +163,235 @@ export default function Respuesta({ datos, intervalo }) {
       return <h1>Sin Fenomeno : {Sol.sinFenomeno}</h1>;
     }
     return (
-      <div>
-        <div>Dia: {tabla.diai}</div>
-        <div>Mes: {tabla.mesi}</div>
-        <div>Año: {tabla.anoi}</div>
-        <div>Hora: {tabla.horut}</div>
-        <h2>Latitud</h2>
-        <div>Grados: {tabla.latgra}</div>
-        <div>Minutos: {tabla.minutos_lat}</div>
-        <div>Orientación: {tabla.latsig}</div>
-        <h2>Longitud</h2>
-        <div>Grados:{tabla.longra}</div>
-        <div>Minutos:{tabla.minutos_lon}</div>
-        <div>Orientacion:{tabla.lonsig}</div>
-  
-        <h2>Sol</h2>
-        <div>Culminacion: {Sol.culminacion}</div>
-        <div>Grados Altura Sol: {Sol.gAlturaSol}</div>
-        <div>Minutos Altura Sol: {Sol.mAlturaSol}</div>
-        <div>Hora Paso Meridiano: {Sol.hPasoMeridiano}</div>
-        <div>Minutos Paso Meridiano: {Sol.mPasoMeridiano}</div>
-  
-        <h1>Fenómenos</h1>
-        <h2>Principio Crepúsculo Náutico</h2>
-        <div>Hora: {Fenomenos.principioCrepNautico.hora1}</div>
-        <div>Minutos: {Fenomenos.principioCrepNautico.minutos1}</div>
-  
-        <h2>Fin Crepúsculo Náutico</h2>
-        <div>Hora: {Fenomenos.finCrepNautico.hora1}</div>
-        <div>Minutos: {Fenomenos.finCrepNautico.minutos1}</div>
-  
-        <h2>Principio Crepúsculo Civil</h2>
-        <div>Hora: {Fenomenos.principioCrepCivil.hora1}</div>
-        <div>Minutos: {Fenomenos.principioCrepCivil.minutos1}</div>
-  
-        <h2>Fin Crepúsculo Civil</h2>
-        <div>Hora: {Fenomenos.finCrepCivil.hora1}</div>
-        <div>Minutos: {Fenomenos.finCrepCivil.minutos1}</div>
-  
-        <h2>Salida</h2>
-        <div>Hora: {Fenomenos.salida.hora1}</div>
-        <div>Minutos: {Fenomenos.salida.minutos1}</div>
-  
-        <h2>Puesta</h2>
-        <div>Hora: {Fenomenos.puesta.hora1}</div>
-        <div>Minutos: {Fenomenos.puesta.minutos1}</div>
-  
-        <h2>Acimutes</h2>
-        <div>Grados Salida Superior: {Fenomenos.Acimutes.gSalidaSuperior}</div>
-        <div>Minutos Salida Superior: {Fenomenos.Acimutes.mASalidaSuperior}</div>
-        <div>Hora Salida Superior: {Fenomenos.Acimutes.hSalidaSuperior}</div>
-        <div>
-          Minutos (Tiempo) Salida Superior: {Fenomenos.Acimutes.mTSalidaSuperior}
+      <div id="contenedor">
+        <div className="fecha">
+          Fecha: {tabla.diai}/{tabla.mesi}/{tabla.anoi}
         </div>
-  
-        <div>Grados Salida Inferior: {Fenomenos.Acimutes.gSalidaInferior}</div>
-        <div>Minutos Salida Inferior: {Fenomenos.Acimutes.mASalidaInferior}</div>
-        <div>Hora Salida Inferior: {Fenomenos.Acimutes.hSalidaInferior}</div>
-        <div>
-          Minutos (Tiempo) Salida Inferior: {Fenomenos.Acimutes.mTSalidaInferior}
-        </div>
-  
-        <div>Grados Puesta Superior: {Fenomenos.Acimutes.gPuestaSuperior}</div>
-        <div>Minutos Puesta Superior: {Fenomenos.Acimutes.mAPuestaSuperior}</div>
-        <div>Hora Salida Superior: {Fenomenos.Acimutes.hSalidaSuperior}</div>
-        <div>
-          Minutos (Tiempo) Salida Superior: {Fenomenos.Acimutes.mTSalidaSuperior}
-        </div>
-  
-        <div>Grados Puesta Inferior: {Fenomenos.Acimutes.gPuestaInferior}</div>
-        <div>Minutos Puesta Inferior: {Fenomenos.Acimutes.mAPuestaInferior}</div>
-        <div>Hora Salida Inferior: {Fenomenos.Acimutes.hSalidaInferior}</div>
-        <div>
-          Minutos (Tiempo) Salida Inferior: {Fenomenos.Acimutes.mTSalidaInferior}
-        </div>
+        <table id="coordenadas">
+          <tr>
+            <th id="header">Hora-UT</th>
+            <td>{tabla.horut}h</td>
+          </tr>
+          <tr>
+            <th id="header">Latitud</th>
+            <td>
+              {tabla.latgra}º {tabla.minutos_lat}' {tabla.latsig}
+            </td>
+          </tr>
+          <tr>
+            <th id="header">Longitud</th>
+            <td>
+              {tabla.longra}º {tabla.minutos_lon}' {tabla.lonsig}
+            </td>
+          </tr>
+        </table>
+
+        <table id="Sol">
+          <tr>
+            <th id="header">Culminacion</th>
+            <td>{Sol.culminacion}</td>
+          </tr>
+          <tr>
+            <th id="header">Grados Altura Sol</th>
+            <td>
+              {Sol.gAlturaSol}º {Sol.mAlturaSol}'
+            </td>
+          </tr>
+          <tr>
+            <th id="header">Hora Paso Meridiano</th>
+            <td>
+              {Sol.hPasoMeridiano}:{Sol.mPasoMeridiano}h
+            </td>
+          </tr>
+        </table>
+
+        <table id="Fenomenos">
+          <tr>
+            <th>Fenómenos</th>
+          </tr>
+          <tr>
+            <table>
+              <th></th>
+              <th>inicio</th>
+              <th>final</th>
+              <tr>
+                <th id="header">Crepúsculo Civil</th>
+                <td>
+                  {Fenomenos.principioCrepCivil.hora1}:
+                  {Fenomenos.principioCrepCivil.minutos1}h
+                </td>
+                <td>
+                  {Fenomenos.finCrepCivil.hora1}:
+                  {Fenomenos.finCrepCivil.minutos1}h
+                </td>
+              </tr>
+              <tr>
+                <th id="header">Crepúsculo Nautico</th>
+                <td>
+                  {Fenomenos.principioCrepNautico.hora1}:
+                  {Fenomenos.principioCrepNautico.minutos1}h
+                </td>
+                <td>
+                  {Fenomenos.finCrepNautico.hora1}:
+                  {Fenomenos.finCrepNautico.minutos1}h
+                </td>
+              </tr>
+              <tr>
+                <th id="header">Salida/Puesta</th>
+                <td>
+                  {Fenomenos.salida.hora1}:{Fenomenos.salida.minutos1}h
+                </td>
+                <td>
+                  {Fenomenos.puesta.hora1}:{Fenomenos.puesta.minutos1}h
+                </td>
+              </tr>
+            </table>
+          </tr>
+        </table>
+
+        <table id="acimutes">
+          <tr>
+            <th>Acimutes</th>
+          </tr>
+          <table>
+            <tr>
+              <th></th>
+              <th>Superior</th>
+              <th>Inferior</th>
+            </tr>
+            <tr>
+              <th id="header">Salida</th>
+              <td>
+                <table id="doble">
+                  <td>
+                    {Fenomenos.Acimutes.gSalidaSuperior}º{" "}
+                    {Fenomenos.Acimutes.mASalidaSuperior}'
+                  </td>
+                  <td>
+                    {Fenomenos.Acimutes.hSalidaSuperior}:
+                    {Fenomenos.Acimutes.mTSalidaSuperior}h
+                  </td>
+                </table>
+              </td>
+              <td>
+                <table id="doble">
+                  <td>
+                    {Fenomenos.Acimutes.gSalidaInferior}º{" "}
+                    {Fenomenos.Acimutes.mASalidaInferior}'
+                  </td>
+                  <td>
+                    {Fenomenos.Acimutes.hSalidaInferior}:
+                    {Fenomenos.Acimutes.mTSalidaInferior}h
+                  </td>
+                </table>
+              </td>
+            </tr>
+            <tr>
+              <th id="header">Puesta</th>
+              <td>
+                <table id="doble">
+                  <td>
+                    {Fenomenos.Acimutes.gPuestaSuperior}º{" "}
+                    {Fenomenos.Acimutes.mAPuestaSuperior}'
+                  </td>
+                  <td>
+                    {Fenomenos.Acimutes.hPuestaSuperior}:
+                    {Fenomenos.Acimutes.mTPuestaSuperior}h
+                  </td>
+                </table>
+              </td>
+              <td>
+                <table id="doble">
+                  <td>
+                    {Fenomenos.Acimutes.gPuestaInferior}º{" "}
+                    {Fenomenos.Acimutes.mAPuestaInferior}'
+                  </td>
+                  <td>
+                    {Fenomenos.Acimutes.hPuestaInferior}:
+                    {Fenomenos.Acimutes.mTPuestaInferior}h
+                  </td>
+                </table>
+              </td>
+            </tr>
+          </table>
+        </table>
       </div>
     );
-  }
-  else if(datos.Luna)
-  {
-    return <h1>Respuesta de Luna</h1>
+  } else if (datos.Luna) {
+    let Luna = datos.Luna;
+    return (
+      <div id="contenedor">
+        <div className="fecha">
+          Fecha: {tabla.diai}/{tabla.mesi}/{tabla.anoi}
+        </div>
+        <table id="coordenadas">
+          <tr>
+            <th id="header">Hora-UT</th>
+            <td>{tabla.horut}h</td>
+          </tr>
+          <tr>
+            <th id="header">Edad (a 0h)</th>
+            <td>{Luna.edad0h}</td>
+          </tr>
+          <tr>
+            <th id="header">Latitud</th>
+            <td>
+              {tabla.latgra}º {tabla.minutos_lat}' {tabla.latsig}
+            </td>
+          </tr>
+          <tr>
+            <th id="header">Longitud</th>
+            <td>
+              {tabla.longra}º {tabla.minutos_lon}' {tabla.lonsig}
+            </td>
+          </tr>
+        </table>
+
+        <table id="Luna">
+          <tr>
+            <th id="header">Culminacion</th>
+            <td>{Luna.culminacion}</td>
+          </tr>
+          <tr>
+            <th id="header">Grados Altura Luna</th>
+            <td>
+              {Luna.gAlturaMeridiano}º {Luna.mAlturaMeridiano}'
+            </td>
+          </tr>
+          <tr>
+            <th id="header">Hora Paso Meridiano</th>
+            <td>
+              {Luna.hPasoMeridiano}:{Luna.mPasoMeridiano}h
+            </td>
+          </tr>
+        </table>
+
+        <table id="acimutes">
+          <tr>
+            <th></th>
+            <th>Grados</th>
+            <th>Hora</th>
+          </tr>
+          <tr>
+            <th id="header">Salida</th>
+            <td>
+              {datos.gAcimutSalida}º {datos.mAcimutSalida}'
+            </td>
+            <td>
+              {datos.hSalida}:{datos.mSalida}h
+            </td>
+          </tr>
+          <tr>
+            <th id="header">Puesta</th>
+            <td>
+              {datos.gAcimutPuesta}º {datos.mAcimutPuesta}'
+            </td>
+            <td>
+              {datos.hPuesta}:{datos.mPuesta}h
+            </td>
+          </tr>
+        </table>
+      </div>
+    );
   }
 }
